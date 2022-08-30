f623900e74426d90b2705f134a15244ea1a42ad5
Moises <moises0peralta@gmail.com>
2022-08-30 15:14:08 -0400

    h

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,21 @@
 // by convention, class names are capitalized
 public class HelloWorld {
 
+    /*
+        this is a multiline block comment
+        more comments here
+     */
     public static void main(String [] args){
         System.out.println("Hello World");
         int x=5;
         boolean z= true;
-        double pi= 3.14159;
+        double pi = 3.14159 ;
+        String name="CS219 techniques of computer science";
+
+        double avagadros=6.022140857e23; //scientific notation
+        double electron_mass=9.1093847015e-31;
+
+
+
     }
 }
