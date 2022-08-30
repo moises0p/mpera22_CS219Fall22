e4459eb30c748f88feecc1b4b54169136afdcb4e
Moises <moises0peralta@gmail.com>
2022-08-30 15:00:19 -0400

    h

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String [] args){
         System.out.println("Hello World");
         int x=5;
-        boolean z=true;
-        double pi=3.14159;
+        boolean z= true;
+        double pi= 3.14159;
     }
 }
